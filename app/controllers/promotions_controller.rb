a80b9fe4b6929bae0130c96bb52af17767cd7011
Filipe Pinato <fpinatoaguiar@gmail.com>
2021-01-27 21:55:55 -0300

    Admin registra uma promoção

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -6,4 +6,19 @@ class PromotionsController < ApplicationController
     def show
         @promotion = Promotion.find(params[:id])
     end
+
+    def new
+    end
+
+    def create
+        @promotion = Promotion.new
+        @promotion.name = params[:promotion][:name]
+        @promotion.description = params[:promotion][:description]
+        @promotion.code = params[:promotion][:code]
+        @promotion.discount_rate = params[:promotion][:discount_rate]
+        @promotion.coupon_quantity = params[:promotion][:coupon_quantity]
+        @promotion.expiration_date = params[:promotion][:expiration_date]
+        @promotion.save
+        redirect_to promotion_path(id: @promotion.id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :promotions, only: [:index, :show]
+  resources :promotions, only: [:index, :show, :new, :create]
 end
